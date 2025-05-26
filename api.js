b78c4120f84ffb38b3d210ecad9079ac839e7787
doyalikess <zachsaliba123@gmail.com>
2025-05-26 19:59:13 +0200

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',  // Replace with your backend URL
+  baseURL: 'https://rsfpper-1-4g28.onrender.com/api',  // Replace with your backend URL
   timeout: 10000,
 });
 
