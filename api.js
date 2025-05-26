7c2b57369f0e8f779827ca2566e0c2bff9871563
doyalikess <zachsaliba123@gmail.com>
2025-05-26 20:46:45 +0200

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://343d-93-166-84-254.ngrok-free.app',  // Replace with your backend URL
+  baseURL: 'https://0116-93-166-84-254.ngrok-free.ap',  // Replace with your backend URL
   timeout: 10000,
 });
 
